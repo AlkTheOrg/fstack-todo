48853237a89b9161388d92ce2a7a893a6f36f8f4
Alkan <alkanorgun@gmail.com>
2022-01-28 23:13:39 +0300

    Sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,113 @@
+import React, { FC, useState, useEffect } from "react";
+import "../styles/Sidebar.scss";
+import { FaChevronLeft } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { combineClassNames } from "../util/combineClassNames";
+import { IconType } from "react-icons";
+
+export interface Props {
+  title?: string;
+  isDetached?: boolean;
+  // DetachIcon?: HTMLImageElement | SVGElement | IconType;
+  DetachIcon?: IconType;
+  closeOnMouseLeave?: boolean;
+  classes: string[];
+  onClose?: () => any;
+}
+
+export const defaultProps: Props = {
+  title: "",
+  isDetached: true,
+  DetachIcon: GiHamburgerMenu,
+  classes: [],
+  onClose: () => {},
+};
+
+const Sidebar: FC<Props> = ({
+  title,
+  isDetached: detached,
+  DetachIcon,
+  classes,
+  onClose,
+  children,
+}): JSX.Element => {
+  const [isDetached, setIsDetached] = useState(detached);
+  const [isVisible, setIsVisible] = useState(!isDetached);
+  //TODO Uncomment below after finishing the design of siebar
+  // const [isVisible, setIsVisible] = useState(true);
+
+  const toggleDetach = () => setIsDetached((prev) => !prev); //TODO handle detach icon click
+
+  const closeSidebar = () => {
+    setIsVisible(false);
+    if (onClose) onClose();
+  }
+
+  const handleMouseLeave = () => {
+    if (isDetached) closeSidebar();
+  };
+
+  const handleMouseEnter = () => {
+    if (!isVisible && isDetached) {
+      setIsVisible(true);
+    }
+  };
+
+  const handleClickHide = () => {
+    if (isDetached) closeSidebar();
+  }
+
+  const combinedClasses = classes ? combineClassNames(classes) : "";
+
+  useEffect(() => {
+    console.log(isVisible);
+  }, [isVisible]);
+
+  return (
+    <aside
+      className={`${combinedClasses} ${isVisible ? "isVisible" : ""} ${
+        isDetached ? "isDetached" : "isAttached"
+      }`}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className="header">
+        {title ? (
+          <h2>{title}</h2>
+        ) : (
+          <div className="invisible-box" aria-hidden="true"></div>
+        )}
+        <div
+          className="detach-btn-wrapper"
+          aria-label="Hide sidebar"
+          role="button"
+          onClick={toggleDetach}
+        >
+          {DetachIcon && <DetachIcon size="25" />}
+        </div>
+      </div>
+      {isDetached && (
+        <div
+          className="hide-btn-container"
+          onMouseEnter={handleMouseEnter}
+          aria-hidden="true"
+        >
+          <div
+            className="hide-btn"
+            role="button"
+            aria-label={
+              isVisible ? "Hide Todo Pages sidebar" : "Show Todo Pages sidebar"
+            }
+            onClick={handleClickHide}
+          >
+            <FaChevronLeft className="hide-icn" />
+          </div>
+        </div>
+      )}
+      {children}
+    </aside>
+  );
+};
+
+Sidebar.defaultProps = defaultProps;
+
+export default Sidebar;
---
+++ src/components/__tests__/Sidebar.test.tsx
@@ -0,0 +1,87 @@
+import { fireEvent, prettyDOM, render, screen } from "@testing-library/react";
+import Sidebar, { Props, defaultProps } from "../Sidebar";
+
+describe("<Sidebar />", () => {
+  const renderSidebar = (
+    props: Props = defaultProps,
+    children?: React.ReactNode
+  ) => {
+    return render(<Sidebar {...props}>{children}</Sidebar>);
+  };
+  /* eslint-disable testing-library/no-node-access */
+
+  it("renders sidebar with given classes", () => {
+    renderSidebar({ ...defaultProps, classes: ["abcd"] });
+    const sidebar = screen.getByRole("complementary");
+    expect(sidebar).toBeInTheDocument();
+    expect(sidebar.classList).toContain("abcd");
+  });
+
+  //TODO Uncomment below after finishing the design of siebar
+  // it("should be detached amd be invisible by default", () => {
+  //   renderSidebar();
+  //   const sidebar = screen.getByRole("complementary");
+  //   expect(sidebar.classList).toContain("isDetached");
+  //   expect(sidebar.classList).not.toContain("isVisible");
+  // });
+
+  // should render the passed title
+  it("should render the passed title", () => {
+    renderSidebar(defaultProps, <h1>Headingh1</h1>);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading.textContent).toBe("Headingh1");
+  });
+
+  it("should render an invisible box when title is not passed", () => {
+    renderSidebar();
+    const invisibleBox = document.querySelector(".invisible-box");
+    // console.log(prettyDOM(invisibleBox))
+
+    expect(invisibleBox?.getAttribute("aria-hidden")).toBe("true");
+    expect(invisibleBox?.textContent).toBe("");
+  });
+
+  it("should be visible when rendered in attached mode", () => {
+    renderSidebar({ ...defaultProps, isDetached: false });
+    const sidebar = screen.getByRole("complementary");
+    expect(sidebar.classList).toContain("isVisible");
+    expect(sidebar.classList).toContain("isAttached");
+  });
+
+  it("should switch to dettached mode when clicked to detach btn", async () => {
+    renderSidebar({ ...defaultProps, isDetached: false });
+    const sidebar = screen.getByRole("complementary");
+
+    const detachBtn = screen.getByRole("button");
+    fireEvent.click(detachBtn);
+
+    expect(sidebar.classList).not.toContain("isAttached");
+  });
+
+  it("should open/close the sidebar on mouse enter/leave", () => {
+    renderSidebar({ ...defaultProps, isDetached: true});
+    const sidebar = screen.getByRole("complementary");
+    const hiderDiv = document.querySelector('.hide-btn-container') as HTMLDivElement;
+    expect(sidebar.classList).not.toContain("isVisible");
+
+    fireEvent.mouseEnter(hiderDiv);
+    expect(sidebar.classList).toContain("isVisible");
+
+    fireEvent.mouseLeave(sidebar);
+    expect(sidebar.classList).not.toContain("isVisible");
+  });
+
+  it("should close the sidebar when clicked to hide icon", () => {
+    renderSidebar();
+    const sidebar = screen.getByRole("complementary");
+    const hiderDiv = document.querySelector('.hide-btn-container') as HTMLDivElement;
+    const hideBtn = document.querySelector('.hide-btn') as HTMLButtonElement;
+
+    fireEvent.mouseEnter(hiderDiv);
+    expect(sidebar.classList).toContain("isVisible");
+
+    fireEvent.click(hideBtn);
+    expect(sidebar.classList).not.toContain("isVisible");
+  })
+});
