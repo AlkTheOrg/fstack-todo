9d79da68fc41ca04b70b91033ea4e2d75781204f
Alkan <alkanorgun@gmail.com>
2022-01-29 18:42:38 +0300

    getXDaysLater fix

--- src/util/getXDaysLater.ts
+++ src/util/getXDaysLater.ts
@@ -1,3 +1,3 @@
-export const getXDaysLater = (date: Date, x: number) => {
+export const getXDaysLater: (date: Date, x: number) => Date = (date, x) => {
   return new Date(date.getFullYear(), date.getMonth(), date.getDate() + x);
 }
