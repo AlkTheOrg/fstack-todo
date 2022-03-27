6e570bd0ff2fd7cbd7c5bbaacacbb130ddab4237
Alkan <alkanorgun@gmail.com>
2022-03-27 12:06:00 +0300

    PopoverMenu

---
+++ src/components/PopoverMenu.tsx
@@ -0,0 +1,69 @@
+import {
+  FC,
+  useRef,
+  useEffect,
+  Dispatch,
+  SetStateAction,
+  useCallback,
+} from "react";
+import "../styles/PopoverMenu.scss";
+
+export type Props = {
+  baseClass: string;
+  popoverClass: string;
+  popoverWrapperClass: string;
+  showPopover: boolean;
+  setShowPopover: Dispatch<SetStateAction<boolean>>;
+  closOnClickOutside?: boolean;
+  popovers: [string, () => any][];
+};
+
+const PopoverMenu: FC<Props> = ({
+  baseClass,
+  popoverClass,
+  popoverWrapperClass,
+  showPopover,
+  setShowPopover,
+  closOnClickOutside = true,
+  children,
+  popovers,
+}) => {
+  const popoverRef = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = useCallback(
+    (e: Event) => {
+      const target = e.target as HTMLElement;
+      const firstChild = popoverRef && popoverRef.current;
+      if (closOnClickOutside && firstChild && !firstChild.contains(target)) {
+        setShowPopover(false);
+      }
+    },
+    [closOnClickOutside, setShowPopover]
+  );
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [handleClickOutside]);
+
+  return (
+    <div
+      ref={popoverRef}
+      className={baseClass + `${showPopover ? " isShow" : ""}`}
+    >
+      {popovers.map(([str, f], i) => {
+        console.log(str);
+        console.log(f);
+        return <div key={"pop-"+i} className={popoverWrapperClass} onClick={f}>
+          <div className={popoverClass}>
+            <p>{str}</p>
+          </div>
+        </div>
+      })}
+    </div>
+  );
+};
+
+export default PopoverMenu;
