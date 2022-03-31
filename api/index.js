7eea6c914e1d2dd19c9ed0925ee251ef3c77cdbc
Alkan <alkanorgun@gmail.com>
2022-04-01 00:36:18 +0300

    dotenv configuration

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 5000
 app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(5000, () => console.log("Listening at port 5000"));
+app.listen(PORT, () => console.log(`Listening at port ${PORT}`));
